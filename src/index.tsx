88dbf850c498faa47fa2a4931fc6314c4dafeec4
JonathasLopes <jon.athas@hotmail.com>
2025-01-30 21:06:12 -0300

    @media added

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './assets/Pallete.css';
 import './assets/Global.css';
+import './assets/@media.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
