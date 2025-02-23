f621bb437bfdeb69be1bf871f56dfec1aa076dfd
JonathasLopes <jon.athas@hotmail.com>
2025-02-22 21:41:54 -0300

    WIP: confirm presence

--- src/pages/Confirm/Confirm.tsx
+++ src/pages/Confirm/Confirm.tsx
@@ -1,4 +1,3 @@
-import { FiSearch } from 'react-icons/fi';
 import './Confirm.css';
 import { useState } from 'react';
 import Collapse from '../../components/Collapse/Collapse';
@@ -30,26 +29,14 @@ function Confirm() {
     return (
         <div className="container-confirm">
             {loading && <Loading />}
-            <div className='container-msg'>
-                <h3>Confirme sua Presença!</h3>
-                <p>
-                    Digite seu nome no campo abaixo, caso esteja em nossa lista de convidados,
-                    irá aparecer seu nome, juntamente com todos os integrantes de seu convite.
-                    Assim, será possível confirmar a sua presença e a das pessoas que irão junto com você!
-                    <br />
-                    <br />
-                    <span className='obs-confirm'>Antes de confirmar sua presença, verifique se realmente é você!</span>
-                    <br />
-                    <br />
-                    <span className='warn-confirm'>***Atenção: Não será permitida a entrada de pessoas que não estejam no convite
-                        e também de quem não confirmou presença até 40 dias antes do evento!***</span>
-                </p>
+            <div className="img-main">
+                <h1>Bianca & Jonathas</h1>
             </div>
             <div className='container-invites'>
-                <div className='container-input'>
-                    <input placeholder='Digite aqui seu nome...' value={search} onChange={e => setSearch(e.target.value)} />
-                    <button disabled={!search || search === ""} type='button' onClick={getByName}><FiSearch /></button>
-                </div>
+                <h3>Confirme sua presença!</h3>
+                <p>Para confirmar a sua presença no casamento só precisa escrever o seu nome e clicar em Pesquisar. Aparecerá o seu nome e só terá que dizer se estará ou não no casamento, além disso, você poderá já confirmar as pessoas que foram convidadas junto com você!</p>
+                <input placeholder='Nome' value={search} onChange={e => setSearch(e.target.value)} />
+                <button onClick={getByName} disabled={search === ""} className='btn-search'>Buscar</button>
                 <div className='container-collapse'>
                     {families.map((family, index) => {
                         return (
