9855f712057a14c95bdd8426759b19726fc86135
JonathasLopes <jon.athas@hotmail.com>
2025-03-11 19:47:05 -0300

    pix added

--- src/components/PixQR/PixQR.tsx
+++ src/components/PixQR/PixQR.tsx
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
+import './PixQR.css';
 
 interface IPixQRProps {
     setShowQR: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-function PixQR({setShowQR }: IPixQRProps) {
+function PixQR({ setShowQR }: IPixQRProps) {
     const closeImage = () => setShowQR(false);
 
     useEffect(() => {
@@ -21,13 +22,15 @@ function PixQR({setShowQR }: IPixQRProps) {
     return (
         <div className="PixQR-container">
             <div className="modal" onClick={closeImage}>
-                <h1>Nosso QR Code para realizar o PIX!</h1>
-                <img src="/images/qrcode.jpg" alt="Imagem Ampliada" className="modal-img" />
-                <h3>Caso não tenham pix, podem fazer transferência também!</h3>
-                <h3>Dados para transferência:</h3>
-                <h4>CC: 14825-4</h4>
-                <h4>Ag: 8706</h4>
-                <h4>Banco: Itaú Unibanco</h4>
+                <div className="modal-pix-container">
+                    <h1>Nosso QR Code para realizar o PIX!</h1>
+                    <img src="/images/qrcode.jpg" alt="Imagem Ampliada" className="modal-img" />
+                    <h3>Caso não tenham pix, podem fazer transferência também!</h3>
+                    <h3>Dados para transferência:</h3>
+                    <h4>CC: 14825-4</h4>
+                    <h4>Ag: 8706</h4>
+                    <h4>Banco: Itaú Unibanco</h4>
+                </div>
             </div>
         </div>
     )
