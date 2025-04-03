a04bdf347d3d943e827e8789d47d7b6553dfd9d7
JonathasLopes <jon.athas@hotmail.com>
2025-04-03 12:27:52 -0300

    messages added

--- src/apis/MessagesAPI.ts
+++ src/apis/MessagesAPI.ts
@@ -1,3 +1,4 @@
+import { IMessageResponse } from "@/interfaces/IResponseMessages";
 import { api } from "../configurations/Axios";
 
 const SendMessage = async (name: string, message: string) => {
@@ -11,6 +12,18 @@ const SendMessage = async (name: string, message: string) => {
     }
 }
 
+const GetAllMessages = async () => {
+    try {
+        const result = await api.get(`GetAllMessages`);
+        const response = await result.data as IMessageResponse[];
+        return response;
+    }
+    catch(error: any) {
+        throw new Error(error.response.data);
+    }
+}
+
 export {
-    SendMessage
+    SendMessage,
+    GetAllMessages
 }
